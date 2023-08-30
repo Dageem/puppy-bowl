2e906347aa485874860e95482b39298e9b0ab332
dageem <dagtin1@gmail.com>
2023-08-29 20:35:18 -0400

    done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Use the API_URL variable to make fetch requests to the API.
 // Replace the placeholder with your cohort name (ex: 2109-UNF-HY-WEB-PT)
 const cohortName = "YOUR COHORT NAME HERE";
-const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
+const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/2307-fsa-et-web-ft-sf/players/`;
 
 /**
  * Fetches all players from the API.
@@ -9,7 +9,9 @@ const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
  */
 const fetchAllPlayers = async () => {
   try {
-    // TODO
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data.data.players;
   } catch (err) {
     console.error("Uh oh, trouble fetching players!", err);
   }
@@ -22,59 +24,60 @@ const fetchAllPlayers = async () => {
  */
 const fetchSinglePlayer = async (playerId) => {
   try {
-    // TODO
+    const response = await fetch(API_URL + playerId + "/");
+    const data = await response.json();
+    return data.data.player;
   } catch (err) {
     console.error(`Oh no, trouble fetching player #${playerId}!`, err);
   }
 };
+fetchSinglePlayer(1)
+function createElement(dt, idx) {
+  this.element = document.createElement("div");
+  this.names = document.createElement("h1");
+  this.names.innerHTML = dt.name;
+  this.id = document.createElement("h2");
+  this.id.innerHTML = dt.id;
+  this.imageUrl = document.createElement("img");
+  this.imageUrl.src=dt.imageUrl;
+  this.element.addEventListener("click", function () {
+    renderSinglePlayer(idx);
+  });
 
-/**
- * Updates `<main>` to display a list of all players.
- *
- * If there are no players, a corresponding message is displayed instead.
- *
- * Each player is displayed in a card with the following information:
- * - name
- * - id
- * - image (with alt text of the player's name)
- *
- * Additionally, each card has two buttons:
- * - "See details" button that, when clicked, calls `renderSinglePlayer` to
- *    display more information about the player
- * - "Remove from roster" button that, when clicked, will call `removePlayer` to
- *    remove that specific player and then re-render all players
- *
- * Note: this function should replace the current contents of `<main>`, not append to it.
- * @param {Object[]} playerList - an array of player objects
- */
+  this.element.appendChild(this.names);
+  this.element.appendChild(this.id);
+  this.element.appendChild(this.imageUrl);
+
+  document.querySelector(".wrapper").appendChild(this.element);
+}
+
 const renderAllPlayers = (playerList) => {
-  // TODO
+  document.querySelector(".wrapper").innerHTML = "";
+  fetchAllPlayers(playerList).then((response) => {
+    response.forEach((i, idx) => {
+      createElement(i, idx + 1);
+    });
+  });
 };
+document.getElementById("all").addEventListener("click", function () {
+  renderAllPlayers();
+});
 
-/**
- * Updates `<main>` to display a single player.
- * The player is displayed in a card with the following information:
- * - name
- * - id
- * - breed
- * - image (with alt text of the player's name)
- * - team name, if the player has one, or "Unassigned"
- *
- * The card also contains a "Back to all players" button that, when clicked,
- * will call `renderAllPlayers` to re-render the full list of players.
- * @param {Object} player an object representing a single player
- */
 const renderSinglePlayer = (player) => {
-  // TODO
+  document.querySelector(".wrapper").innerHTML = "";
+  fetchSinglePlayer(player).then((response) => {
+    createElement(response);
+  });
 };
-
+document.getElementById("One").addEventListener("click", function () {
+  renderSinglePlayer(3);
+});
 /**
  * Initializes the app by fetching all players and rendering them to the DOM.
  */
 const init = async () => {
   const players = await fetchAllPlayers();
   renderAllPlayers(players);
-
 };
 
 // This script will be run using Node when testing, so here we're doing a quick
